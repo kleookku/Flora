4110bbd8b34e260270b4cf0a0dcf1eb0a29d4a88
Kleo Ku <kleoku@fb.com>
2022-06-30 14:18:21 -0700

    flora

---
+++ Flora/Flora/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Flora
+//
+//  Created by Kleo Ku on 6/30/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flora/Flora/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Flora
+//
+//  Created by Kleo Ku on 6/30/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
